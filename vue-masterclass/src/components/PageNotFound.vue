b1b3ad4455aff375daca5ed6e6188d4cdde3ef86
Leo Merlo <lmerlo@perceptyx.com>
2021-12-12 15:12:48 -0300

    Chapter 3 - Routing

---
+++ vue-masterclass/src/components/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-full">
+  <h1>Page not found</h1>
+  <router-link
+    :to="{ name: 'Home' }"
+  >
+    Go back to home
+  </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
